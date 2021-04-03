6e0fa76f1ec35e44daaf467a8b4de5baa8c3f968
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-03 16:25:37 +0200

    Update config

--- config.sh
+++ config.sh
@@ -1,11 +1,11 @@
-export NAMESPACE='ns'
-export PROJECT_NAME='project'
-export SERVICE_ACCOUNT='service-account@project.iam.gserviceaccount.com'
-export KEY_FILE="$HOME/Sites/ansible/project.json"
+export NAMESPACE='hebe'
+export PROJECT_NAME='hebe-299715'
+export SERVICE_ACCOUNT='service-account@hebe-299715.iam.gserviceaccount.com'
+export KEY_FILE="$HOME/Sites/ansible/hebe-299715-fed8d8f694a1.json"
 export IMAGE_TAG='debian-sid'
 export USER='debian'
 export HOST_KEY=$(cat ~/.ssh/id_rsa.pub)
 export REGION='europe-north1' # gcloud compute regions list
 export ZONE='europe-north1-c' # gcloud compute zones list
 export MACHINE_TYPE='n1-standard-8' # gcloud compute machine-types list --zones=$ZONE
-export CLUSTER_NAME='kibana'
+export CLUSTER_NAME='app'
