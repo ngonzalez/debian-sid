816051bf0f7e227dd3863b97d6706317003c5cbc
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-05 14:47:15 +0200

    Update config

--- config.sh
+++ config.sh
@@ -1,11 +1,14 @@
-export NAMESPACE='ns'
-export PROJECT_NAME='project'
-export SERVICE_ACCOUNT='service-account@project.iam.gserviceaccount.com'
-export KEY_FILE="$HOME/Sites/ansible/project.json"
+# docker
 export IMAGE_TAG='debian-sid'
 export USER='debian'
 export HOST_KEY=$(cat ~/.ssh/id_rsa.pub)
+
+# gcp
+export PROJECT_NAME='project'
+export NAMESPACE='namespace'
+export SERVICE_ACCOUNT='service-account@project.iam.gserviceaccount.com'
+export KEY_FILE="$HOME/.project_key.json"
 export REGION='europe-north1' # gcloud compute regions list
 export ZONE='europe-north1-c' # gcloud compute zones list
 export MACHINE_TYPE='n1-standard-8' # gcloud compute machine-types list --zones=$ZONE
-export CLUSTER_NAME='kibana'
+export CLUSTER_NAME='app'
