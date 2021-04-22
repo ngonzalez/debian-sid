6617cfe1e8784b91903d1a8c6f23ceac2a993ab9
Nicolas Gonzalez <nicolasgonzalez180@gmail.com>
2021-04-22 21:54:20 +0200

    Update README, config

--- config.sh
+++ config.sh
@@ -1,7 +1,4 @@
 # docker
-export IMAGE_TAG='debian-sid'
+export IMAGE_TAG='ngonzalez121/debian-sid'
 export USER='debian'
 export HOST_KEY=$(cat ~/.ssh/id_rsa.pub)
-
-# gcp
-export PROJECT_NAME='project'
